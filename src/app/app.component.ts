86fb19f3558f1683b8a62314c019def23b850991
yigitfindikli <yigitfindikli@gmail.com>
2018-07-18 15:00:06 +0300

    Icon code show

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,17 +9,25 @@ import { IconService } from './service/icon.service';
 export class AppComponent implements OnInit {
   title = 'app';
   icons:object[];
-  selectedIcon:object;
+  selectedIcon:any;
+  iconCode= '';
   constructor(private service:IconService){}
   getIcons(){
     this.service.getIcons().subscribe((data:any)=>{
       this.icons=data;
-    })
+    });
   }
   getIcon(index:number){
     this.selectedIcon=this.service.getIcon(index);
+    this.getIconString();
+  }
+  getIconString(){
+    if(this.selectedIcon){
+      this.iconCode='<i class="'+this.selectedIcon.class+' "></i>';
+    }
   }
   ngOnInit(){
   this.getIcons();
+  this.selectedIcon=this.service.getIcon(0);
   }
 }
