6905f332c5b898d5d24e44726801745be38d7ff1
yigitfindikli <yigitfindikli@gmail.com>
2018-07-25 14:56:50 +0300

    unselected added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { IconService } from './service/icon.service';
 
 @Component({
@@ -6,29 +6,32 @@ import { IconService } from './service/icon.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent implements OnInit {
   title = 'app';
-  icons:object[];
-  selectedIcon:any;
-  iconCode= '';
-  constructor(private service:IconService){}
-  getIcons(){
-    this.service.getIcons().subscribe((data:any)=>{
-      this.icons=data;
-      this.selectedIcon=this.icons[0];
+  icons: object [];
+  selectedIcon: any;
+  iconCode = '';
+  constructor(private service: IconService) {}
+  getIcons() {
+    this.service.getIcons().subscribe((data: any) => {
+      this.icons = data;
       this.getIconString();
     });
   }
-  getIcon(id:any){
-    this.selectedIcon=this.service.getIcon(id);
+  getIcon(id: any) {
+    this.selectedIcon = this.service.getIcon(id);
     this.getIconString();
   }
-  getIconString(){
-    if(this.selectedIcon){
-      this.iconCode='<i class="pi pi-'+this.selectedIcon.properties.name+'"></i>';
+  getIconString() {
+    if (this.selectedIcon) {
+      this.iconCode = '<i class="pi pi-' + this.selectedIcon.properties.name + '"></i>';
     }
   }
-  ngOnInit(){
+  unselectIcon() {
+    this.selectedIcon = null;
+  }
+  ngOnInit() {
   this.getIcons();
   }
 }
