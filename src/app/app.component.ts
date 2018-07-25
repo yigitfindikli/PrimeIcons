933dbdadbaeada6722c2a4d0b712e1cb011a5a02
yigitfindikli <yigitfindikli@gmail.com>
2018-07-25 16:37:16 +0300

    animations added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { IconService } from './service/icon.service';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('popOverState', [
+      state('show', style({
+        opacity: 1
+      })),
+      state('hide',   style({
+        opacity: 0
+      })),
+      transition('show => hide', animate('300ms ease-out')),
+      transition('hide => show', animate('500ms ease-in'))
+    ])
+  ]
 })
 
 export class AppComponent implements OnInit {
@@ -12,15 +25,24 @@ export class AppComponent implements OnInit {
   icons: object [];
   selectedIcon: any;
   iconCode = '';
+  show = false;
   constructor(private service: IconService) {}
+
+  get stateName() {
+    return this.show ? 'show' : 'hide'
+  }
+
+
+
   getIcons() {
     this.service.getIcons().subscribe((data: any) => {
       this.icons = data;
       this.getIconString();
     });
   }
-  getIcon(id: any) {
+  getIcon(id) {
     this.selectedIcon = this.service.getIcon(id);
+    this.show=true;
     this.getIconString();
   }
   getIconString() {
@@ -30,6 +52,7 @@ export class AppComponent implements OnInit {
   }
   unselectIcon() {
     this.selectedIcon = null;
+    this.show=false;
   }
   ngOnInit() {
   this.getIcons();
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule }   from '@angular/forms';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { IconService } from './service/icon.service';
@@ -17,7 +17,8 @@ import { FilterPipe } from './shared/filter.pipe';
   imports: [
     BrowserModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [IconService],
   bootstrap: [AppComponent]
